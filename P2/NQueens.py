637b83499a02ec5e10c698af1d367e4d8531dd43
Omar Khashoggi <omar.w.kh@gmail.com>
2017-03-14 00:59:54 +0300

    isSolved, maxConflicts, no queens copy, limit

--- P2/NQueens.py
+++ P2/NQueens.py
@@ -9,20 +9,30 @@ def getRandomAssignment(n):
     row = queens[col]
     top left is (0, 0)
     '''
-    return random.sample(range(0, n), n)
+    return random.sample(xrange(0, n), n)
 
-def getFitness(queens):
-    fitness = 0
-
+def isSolved(queens):
     for column in range(0, len(queens)):
-        fitness += numberOfConfilct(column, queens, True)
+        if hasConflict(column, queens, True):
+            return False
+    return True
 
-    return fitness
-
-def numberOfConfilct(indexOfQueen, queens, excludePrevious):
-    conflicts = 0
+def hasConflict(indexOfQueen, queens, excludePrevious):
     queen = (indexOfQueen, queens[indexOfQueen])
     indexOfNext = indexOfQueen + 1 if excludePrevious else 0
+
+    for col, row in enumerate(queens[indexOfNext:], indexOfNext):
+        otherQueen = (col, row)
+        if sameRow(queen, otherQueen):
+            return True
+        if sameDiagonal(queen, otherQueen):
+            return True
+    return False
+
+def numberOfConfilct(indexOfQueen, queens, maxConflicts=sys.maxint):
+    conflicts = 0
+    queen = (indexOfQueen, queens[indexOfQueen])
+    indexOfNext = indexOfQueen + 1
 
     # (R)ight, (L)eft, U(p), D(own)
     rowR = False
@@ -37,34 +47,27 @@ def numberOfConfilct(indexOfQueen, queens, excludePrevious):
         allTrue = rowR and rowL and diagonalUR and diagonalUL and diagonalDR and diagonalDL
         if allTrue:
             break
+        if conflicts > maxConflicts:
+            return sys.maxint
         if not rowR:
             rowR = sameRow(queen, otherQueen) and queen[0] < otherQueen[0]
+            conflicts += 1 if rowR else 0
         if not rowL:
             rowL = sameRow(queen, otherQueen) and queen[0] > otherQueen[0]
+            conflicts += 1 if rowL else 0
         if not diagonalUR:
             diagonalUR = sameDiagonal(queen, otherQueen) and queen[0] < otherQueen[0] and queen[1] > otherQueen[1]
+            conflicts += 1 if diagonalUR else 0
         if not diagonalDR:
             diagonalDR = sameDiagonal(queen, otherQueen) and queen[0] < otherQueen[0] and  queen[1] < otherQueen[1]
+            conflicts += 1 if diagonalDR else 0
         if not diagonalUL:
             diagonalUL = sameDiagonal(queen, otherQueen) and queen[0] > otherQueen[0] and queen[1] > otherQueen[1]
+            conflicts += 1 if diagonalUL else 0
         if not diagonalDL:
             diagonalDL = sameDiagonal(queen, otherQueen) and queen[0] > otherQueen[0] and queen[1] < otherQueen[1]
-    conflicts = int(rowR) + int(rowL) + int(diagonalUR) + int(diagonalUL) + int(diagonalDR) + int(diagonalDL)
+            conflicts += 1 if diagonalDL else 0
     return conflicts
-'''
-    sameRow = False
-    sameDiagonal = False
-    for col, row in enumerate(queens[indexOfNext:], indexOfNext):
-        if(queen != otherQueen):
-            if not sameRow:
-                sameRow = sameRow(queen, otherQueen)
-            if not sameDiagonal:
-                sameDiagonal = sameDiagonal(queen, otherQueen)
-            if abs(queen[0] - otherQueen[0]) == abs( queen[1] - otherQueen[1] && ):   # x-x == y-y diagnoal
-                conflicts += 1
-            elif(queen[1] == otherQueen[1]):    # y = y row check
-                conflicts += 1
-'''
 
 def sameRow(queen1, queen2):
     return queen1[1] == queen2[1]
@@ -72,58 +75,64 @@ def sameRow(queen1, queen2):
 def sameDiagonal(queen1, queen2):
     return abs(queen1[0] - queen2[0]) == abs(queen1[1] - queen2[1])
 
-def mutate(queens):
-    queen = random.randrange(0, len(queens))
+def mutate(queens, excludeQueen=None):
+    queen, altQueen = random.sample(queens, 2)
+    queen = queen if queen != excludeQueen else altQueen
     row = minConflictValue(queen, queens)
-    newQueens = list(queens)
-    newQueens[queen] = row
-    return newQueens
+    queens[queen] = row
+    return (queens, queen) # mutated, variable to exclue next
 
 def minConflictValue(queenColumn, queens):# n
     minConfilct = sys.maxint
-    queens = list(queens)
     minRows = []
     for row in range(0, len(queens)):
         queens[queenColumn] = row
-        numConflicts = numberOfConfilct(queenColumn, queens, False)
+        # only check affected
+        numConflicts = numberOfConfilct(queenColumn, queens, minConfilct)
         if numConflicts < minConfilct:
             minConfilct = numConflicts
             minRows = [row]
         elif numConflicts == minConfilct:
             minRows.append(row)
 
-    return random.sample(minRows, 1)[0] #return the min row conficlt
+    return minRows[random.randrange(0, len(minRows))]
 
 def solve(n, doDisplay, initial=None):
     startTime = datetime.now()
     random.seed()
 
+    # not this is useless except for logging/display
     initialQueens = initial or getRandomAssignment(n)
-    current = Configuration(initialQueens)
+    queens = list(initialQueens)
 
     if doDisplay:
         steps = 0
-        display(current, startTime)
+        display(queens, startTime)
     
-    while current.fitness > 0:
-        child = mutate(current.queens)
-        child = Configuration(child)
+    limit = n
+    count = 0
+    resets = 0
+    excludeQueen = None
+    while not isSolved(queens):
+        queens, excludeQueen = mutate(queens, excludeQueen)
+        count =+ 1
+        if count > limit:
+            queens = getRandomAssignment(n)
+            resets += 1
+            count = 0
 
         if doDisplay:
+            display(queens, startTime)
             steps += 1
 
-        if doDisplay:
-            display(child, startTime)
-        current = child
-
     if doDisplay:
-        print 'steps: {0}, initial: {1}'.format(steps, initialQueens)
-    return current
+        print 'steps: {0}, resets: {1}, initial: {2}'.format(steps, resets, initialQueens)
+    
+    return queens
 
-def display(configuration, time):
+def display(queens, time):
     timeDiff = datetime.now() - time
 
-    queens = configuration.queens
     queens_range = range(0, len(queens))
     board = ''
     for row in queens_range:
@@ -133,14 +142,9 @@ def display(configuration, time):
             board += 'Q' if rowQ == row else '.'
         board += '|\n'
 
-    print timeDiff, configuration.fitness
+    print timeDiff
     print queens
     print board
-
-class Configuration:
-    def __init__(self, queens):
-        self.queens = queens
-        self.fitness = getFitness(queens)
 
 class Test(unittest.TestCase):
     def test_4(self):
@@ -150,7 +154,7 @@ class Test(unittest.TestCase):
         self.nQueens(8, True)
 
     def b(self, n):
-        benchmark(lambda: self.nQueens(n, False), 1000)
+        benchmark(lambda: solve(n, False), 1000)
 
     def test_b10(self):
         self.b(10)
@@ -164,10 +168,11 @@ class Test(unittest.TestCase):
     def test_b10000(self):
         self.b(10000)
 
-    def test_fitness(self):
-        queens = Configuration([5, 2, 0, 7, 4, 7, 1, 6])
-        display(queens, datetime.now())
-        self.assertEqual(1, queens.fitness)
+    def test_solved(self):
+        pass
+        #queens = 
+        #display(queens, datetime.now())
+        #self.assertEqual(1, queens.fitness)
 
     def test_minconflict(self):
         queens = [3, 0, 1, 3]
@@ -181,10 +186,9 @@ class Test(unittest.TestCase):
         solve(10, True, queens)
 
     def nQueens(self, n, doDisplay):
-        optimalFitness = 0
         solution = solve(n, doDisplay)
 
-        self.assertEqual(optimalFitness, solution.fitness)
+        self.assertTrue(isSolved(solution))
 
 def benchmark(function, times=100):
     averageTime = 0
